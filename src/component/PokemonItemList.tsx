5b699f6eae1e5eca42e5e8361f756f9e86292603
E214194U <alexandreclenet@free.fr>
2023-12-12 00:07:39 +0100

    display

--- src/component/PokemonItemList.tsx
+++ src/component/PokemonItemList.tsx
@@ -27,23 +27,24 @@ function PokemonListItem({ pokemon }: any) {
         <div className="pokemonItemCard">
           <div className="flex items-center justify-center">
             <img
+            className="absolute w-2/3 top-12"
               src={pokemonDetails.sprites.front_default}
               alt={pokemonDetails.name}
             />
           </div>
-          <div className="text-center">{pokemonDetails.id}</div>
-          <div className="text-center">{pokemonDetails.name}</div>
-          <div className="items-center justify-around">
-            {pokemonDetails.types.map((type: any) => (
-              <img
-                src={`https://play.pokemonshowdown.com/sprites/types/${
-                  type.type.name.charAt(0).toUpperCase() +
-                  type.type.name.slice(1)
-                }.png`}
-                alt={type.type.name}
-                key={type.type.name}
-              />
-            ))}
+          <div className="text-[#b4ebff] text-xl w-4/5 left-[10%] top-[57%] absolute">{pokemonDetails.id.toString().padStart(4, '0')}</div>
+          <div className="text-2xl w-4/5 left-[10%] top-[63%] absolute">{pokemonDetails.name.charAt(0).toUpperCase()+pokemonDetails.name.slice(1)}</div>
+          <div className="w-full flex items-center justify-center absolute bottom-12">
+          {pokemonDetails.types.map((type: any) => (
+            <img
+              src={`https://play.pokemonshowdown.com/sprites/types/${
+                type.type.name.charAt(0).toUpperCase() +
+                type.type.name.slice(1)
+              }.png`}
+              alt={type.type.name}
+              key={type.type.name}
+            />
+          ))}
           </div>
         </div>
       </Link>
